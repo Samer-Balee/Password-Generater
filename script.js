495eb70fdb30409e0d3a21a5b04d76abce90a70f
Samer Balee <samerbalee@gmail.com>
2021-12-14 00:59:13 +1100

    Breaking down the code

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var characters = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+// var resultPassword = [];
+
+var generatePassword = function() {
+
+  var userChoice = window.prompt( "choose number of charactors");
+
+  if (isNaN(userChoice)) {
+
+   window.alert(" Please choose avalid number ")
+
+   return generatePassword();
+
+  }
+  var result = "";
+
+  for (var i = 0 ; i < userChoice ; i ++ ) {
+    var index = Math.floor(Math.random() * characters.length);
+    result += characters[index]
+  }
+  return result;
+
+  // var numPassword = Math.floor(Math.random() * userChoice);
+
+  // var passwor
+
+};
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
