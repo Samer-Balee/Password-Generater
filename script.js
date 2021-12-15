f011ae6b8288252ddd441e55dfb57f7e83227e15
Samer Balee <samerbalee@gmail.com>
2021-12-15 19:34:09 +1100

    Adding comments to sctipt file

--- script.js
+++ script.js
@@ -1,80 +1,81 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowerCaseLeters = "abcdefghijklmnopqrstuvxyz";
+var lowerCaseLeters = "abcdefghijklmnopqrstuvxyz";  // lower case string
 
-var upperCaseLeters = lowerCaseLeters.toLocaleUpperCase();
+var upperCaseLeters = lowerCaseLeters.toLocaleUpperCase(); //upper case string
 
-var numericCharacters = "0123456789";
+var numericCharacters = "0123456789"; // numeric charactors
 
-var symbolCharacters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var symbolCharacters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"; // symbole charactors
 
 
+// main function
 var generatePassword = function() {
 
-  var userNumChoice = window.prompt( "Choose number of charactors between 8 to 128.");
+  var userNumChoice = window.prompt( "Choose number of charactors between 8 to 128."); // using prompt to ask user for number of charactors
   
-  if (isNaN(userNumChoice)) {
+  if (isNaN(userNumChoice)) {  // checking if it's a valid number
 
-   window.alert(" Please enter avalid number ");
+    window.alert(" Please enter avalid number "); // alert function
 
-   return generatePassword();
+    return generatePassword(); // if the number is not valid return up to the function
 
   }
 
-  if ( userNumChoice < 8 || userNumChoice > 128) {
+  if ( userNumChoice < 8 || userNumChoice > 128) {  //checking if the number is between 8 to 128
 
-    window.alert("Please choose a number between 8 to 128 ");
+    window.alert("Please choose a number between 8 to 128 "); // if not return up to function
 
     return generatePassword();
 
   }
 
   
-  var allCharacters = "";
+  var allCharacters = ""; // making empty string to full after user choices
 
-  var  includeLowerCase = window.confirm("Click OK, if you would like to include lower Case letters.");
+  var  includeLowerCase = window.confirm("Click OK, if you would like to include lower Case letters."); // confirmation to add lower case characters
 
-  if (includeLowerCase) {
+  if (includeLowerCase) { // if it's true
 
-    allCharacters += lowerCaseLeters;
+    allCharacters += lowerCaseLeters; // add lower case string to all characters string using +=
   }
 
 
-  var  includeUpperCase = window.confirm("Click OK, if you would like to include upper Case letters.");
+  var  includeUpperCase = window.confirm("Click OK, if you would like to include upper Case letters."); // confirmation to add upper case characters
 
-  if (includeUpperCase) {
+  if (includeUpperCase) {  // if it's true
 
-    allCharacters = allCharacters.concat("" , upperCaseLeters);
+    allCharacters = allCharacters.concat("" , upperCaseLeters); // add upper case string to all characters string using .concat
    
   }
 
-  var  includeNumbers = window.confirm("Click OK, if you would like to include numbers.");
+  var  includeNumbers = window.confirm("Click OK, if you would like to include numbers."); // confirmation to add numeric characters
 
-  if (includeNumbers) {
+  if (includeNumbers) { // if it's true
 
-    allCharacters = allCharacters.concat("" , numericCharacters);
+    allCharacters = allCharacters.concat("" , numericCharacters); // add numeric string to all characters string using .concat
    
   }
 
-  var  includeSymbols = window.confirm("Click OK, if you would like to include symbol charaters.");
+  var  includeSymbols = window.confirm("Click OK, if you would like to include symbol charaters."); // confirmation to add symbol characters
 
-  if (includeSymbols) {
+  if (includeSymbols) { // if it's true
 
-    allCharacters = allCharacters.concat("" , symbolCharacters);
+    allCharacters = allCharacters.concat("" , symbolCharacters);  // add symbols string to all characters string using .concat
    
   }
  
 
-  var result = "";
+  var result = ""; // empty string for generate password
 
-  for (var i = 0 ; i < userNumChoice ; i ++ ) {
+  for (var i = 0 ; i < userNumChoice ; i ++ ) { //for loop to choose random charactor from all charactors string
 
-    var index = Math.floor(Math.random() * allCharacters.length);
+    var index = Math.floor(Math.random() * allCharacters.length); // using math.random function
 
-    result += allCharacters[index];
+    result += allCharacters[index]; // adding the chosen charactor to passwoord string
   }
-  return result;
+  return result; // returning the result
 
  
 };
